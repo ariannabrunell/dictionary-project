7ce228fa2a3a8ee31584f1358ecaf78d05f6b743
ariannabrunell <83625264+ariannabrunell@users.noreply.github.com>
2021-09-22 12:14:29 -0500

    Set up the search engine in a new component, Dictionary.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header text-center">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Arianna Brunell</small>
+        </footer>
+      </div>
     </div>
   );
 }
