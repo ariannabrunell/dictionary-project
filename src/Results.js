a38ddbf9cfc6d91a1ee193cdcbae89bc05f42ab5
ariannabrunell <83625264+ariannabrunell@users.noreply.github.com>
2021-09-22 22:04:55 -0500

    Added phonetics to Results.js

--- src/Results.js
+++ src/Results.js
@@ -6,6 +6,16 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+
+        <a
+          href={props.results.phonetics[0].audio}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Listen
+        </a>
+        <br />
+        {props.results.phonetics[0].text}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
