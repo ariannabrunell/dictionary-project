4815315b79ddf1bbf68c0fb3b159d4226ed7428e
ariannabrunell <83625264+ariannabrunell@users.noreply.github.com>
2021-09-22 19:16:18 -0500

    Created Synonyms component, looped through data

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,7 +1,7 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
@@ -9,12 +9,14 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <br />
-              <em>{definition.example}</em>
-            </p>
+            <strong>Definition:</strong>
+            {definition.definition}
+            <br />
+            <br />
+            <strong>Example:</strong>
+            <em>{definition.example}</em>
+            <br />
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
