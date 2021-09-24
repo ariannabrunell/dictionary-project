0068d25425916d7481b95a860059491ed9e16cbd
ariannabrunell <83625264+ariannabrunell@users.noreply.github.com>
2021-09-23 20:59:05 -0500

    Added default keyword

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         <header className="App-header text-center">DIC·TION·AR·Y</header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
           <small>
